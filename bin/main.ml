e0d9aca350933ed3c7d53c3d9e9c802274067860
marius <marnyg@proton.me>
2023-12-02 18:25:37 +0000

    adding nix check test

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,11 @@
 let () = print_endline "Hello, World!"
+
+(* read file  *)
+(* move fundtion to lib *)
+(**)
+(* do function on each  line *)
+(* find first and last nuber *)
+
+
+
+
--- flake.nix
+++ flake.nix
@@ -9,13 +9,22 @@
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
       myPro = (opam-nix.lib.x86_64-linux.buildDuneProject { } "myPro" ./. { ocaml-base-compiler = "*"; }).myPro;
       myShell = pkgs.mkShell {
-        buildInputs =  [ myPro pkgs.nixd pkgs.rnix-lsp ];
+        buildInputs = [ myPro pkgs.nixd pkgs.rnix-lsp ];
         LSP_SERVERS = "ocamllsp, rnix, nixd";
       };
     in
     {
       packages.x86_64-linux.default = myPro;
-      apps.x86_64-linux.default = { type = "app"; program = "${myPro}/bin/main"; };
+      apps.x86_64-linux.default = { type = "app"; program = "${myPro}/bin/myPro"; };
+      checks.x86_64-linux.tests = pkgs.stdenv.mkDerivation {
+        name = "dune-test";
+        buildInputs = [ myPro ];
+        src = ./.;
+        checkPhase = "dune test && touch $out/ok ";
+        installPhase = "echo No installation needed for test && mkdir -p $out && touch $out/testOK";
+      };
+
+
       formatter.x86_64-linux = pkgs.nixpkgs-fmt;
       devShells.x86_64-linux.default = myShell;
     };
